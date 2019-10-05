7b6793a1115058fed6ffaccd15e795e9e7cf6426
Alex Reznichenko <alex.reznichenko789@gmail.com>
2019-10-05 12:18:45 +0300

    Prepare for deploy

--- server.js
+++ server.js
@@ -14,9 +14,11 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
-app.use(express.static(path.resolve(__dirname, "client", "build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "client", "build")));
 
-app.get("*", (req, res) =>
+  app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-);
+  );
+}
 app.listen(process.env.PORT || 8000, () => {});
